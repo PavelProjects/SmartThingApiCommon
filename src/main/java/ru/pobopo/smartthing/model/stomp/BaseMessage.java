1ad54b3a34a3ab1dc2a6e64c6e2704348a59f425
PavelProjects <paveloalexeev52@gmail.com>
2024-04-27 22:37:59 +0300

    fixes

--- src/main/java/ru/pobopo/smartthing/model/stomp/BaseMessage.java
+++ src/main/java/ru/pobopo/smartthing/model/stomp/BaseMessage.java
@@ -1,16 +1,19 @@
 package ru.pobopo.smartthing.model.stomp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.UUID;
 
 @Data
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseMessage {
     private UUID id;
-    private final GatewayMessageType type;
-    private boolean needResponse = true;
+    private GatewayMessageType type;
 
     public BaseMessage(GatewayMessageType type) {
         this.type = type;
