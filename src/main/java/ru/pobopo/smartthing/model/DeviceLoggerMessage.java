be4b1383817e7d9829acb79b8ec7f48bef5f1cc7
PavelProjects <paveloalexeev52@gmail.com>
2024-08-24 21:54:33 +0300

    logs processing rework

--- src/main/java/ru/pobopo/smartthing/model/DeviceLoggerMessage.java
+++ src/main/java/ru/pobopo/smartthing/model/DeviceLoggerMessage.java
@@ -2,17 +2,26 @@ package ru.pobopo.smartthing.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
 import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 @ToString
 @Schema(description = "Device log message")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DeviceLoggerMessage {
+    private final static Pattern messagePattern = Pattern.compile("(?<name>.+)[&](?<level>.+)[&](?<tag>.+)[&](?<message>.+)\\Z");
+    private static final Logger log = LoggerFactory.getLogger(DeviceLoggerMessage.class);
+
     @Schema(description = "Message receive date time")
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateTime;
@@ -26,26 +35,4 @@ public class DeviceLoggerMessage {
     private String message;
     @Schema(description = "Where messages were sent - tcp or multicast channel")
     private DeviceLogSource source;
-
-    public static DeviceLoggerMessage parse(String address, String message) {
-        DeviceLoggerMessage deviceLoggerMessage = new DeviceLoggerMessage();
-        if (StringUtils.isBlank(message)) {
-            return deviceLoggerMessage;
-        }
-
-        String[] splited = message.split("_&_*");
-        if (splited.length != 4) {
-            return deviceLoggerMessage;
-        }
-
-        deviceLoggerMessage.setDateTime(LocalDateTime.now());
-        deviceLoggerMessage.setDevice(
-            new DeviceInfo(address, splited[0])
-        );
-        int level = Integer.parseInt(splited[1]);
-        deviceLoggerMessage.setLevel(Level.intToLevel(level));
-        deviceLoggerMessage.setTag(splited[2]);
-        deviceLoggerMessage.setMessage(splited[3].trim());
-        return deviceLoggerMessage;
-    }
 }
