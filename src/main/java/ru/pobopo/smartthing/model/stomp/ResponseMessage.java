0362c4ff589be7d28001649a18d9d00a88827d61
PavelProjects <paveloalexeev52@gmail.com>
2024-09-24 00:35:11 +0300

    response message data type update

--- src/main/java/ru/pobopo/smartthing/model/stomp/ResponseMessage.java
+++ src/main/java/ru/pobopo/smartthing/model/stomp/ResponseMessage.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+
 import java.util.UUID;
 
 @Data
@@ -12,7 +14,7 @@ import java.util.UUID;
 @Builder
 public class ResponseMessage {
     private UUID requestId;
-    private Object data;
+    private ResponseEntity<String> data;
     private String error;
     private String stack;
     private boolean success = true;
