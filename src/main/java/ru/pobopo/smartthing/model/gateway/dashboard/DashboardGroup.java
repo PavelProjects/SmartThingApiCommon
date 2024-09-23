6541884716d4ea2cf0b94cd072cdced49961b1e4
PavelProjects <paveloalexeev52@gmail.com>
2024-09-23 22:42:38 +0300

    DashboardGroup added builder

--- src/main/java/ru/pobopo/smartthing/model/gateway/dashboard/DashboardGroup.java
+++ src/main/java/ru/pobopo/smartthing/model/gateway/dashboard/DashboardGroup.java
@@ -1,6 +1,9 @@
 package ru.pobopo.smartthing.model.gateway.dashboard;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.pobopo.smartthing.model.DeviceInfo;
 
 import java.util.ArrayList;
@@ -8,6 +11,9 @@ import java.util.List;
 import java.util.UUID;
 
 @Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class DashboardGroup {
     private UUID id;
     private DeviceInfo device;
