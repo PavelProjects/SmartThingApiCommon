ed90f46a8a72a59ddabd2bc063cf6b182f14ee00
PavelProjects <paveloalexeev52@gmail.com>
2024-09-24 17:45:10 +0300

    Revert "removed deprecated InternalHttpResponse"
    This reverts commit b8bb85e7

---
+++ src/main/java/ru/pobopo/smartthing/model/InternalHttpResponse.java
@@ -0,0 +1,25 @@
+package ru.pobopo.smartthing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InternalHttpResponse implements Serializable {
+    private HttpStatusCode status;
+    private String data;
+    private HttpHeaders headers;
+
+    public ResponseEntity<String> toResponseEntity() {
+        return new ResponseEntity<>(data, headers, status);
+    }
+}
