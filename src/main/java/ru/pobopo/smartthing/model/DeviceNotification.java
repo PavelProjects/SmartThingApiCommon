70fc4f6a9f9d7d62be3e7442c4f63869507ae91d
PavelProjects <paveloalexeev52@gmail.com>
2024-10-28 22:07:26 +0300

    added date in device notification

--- src/main/java/ru/pobopo/smartthing/model/DeviceNotification.java
+++ src/main/java/ru/pobopo/smartthing/model/DeviceNotification.java
@@ -1,9 +1,12 @@
 package ru.pobopo.smartthing.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +15,6 @@ import lombok.NoArgsConstructor;
 public class DeviceNotification {
     private DeviceInfo device;
     private Notification notification;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateTime = LocalDateTime.now();
 }
